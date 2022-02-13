a5932479dcf2a340ad76d068cd250a45bc9881c3
Diego Fabián Ledesma Motta <53279310+DiegoFLM@users.noreply.github.com>
2022-02-12 21:55:26 -0500

    queries.sql primera versión

---
+++ queries.sql
@@ -0,0 +1,25 @@
+
+
+--3.3.a   Encuentre el dept, title de los instructores registrados en la base de datos.
+SELECT dept, title 
+FROM instructor;
+
+
+--3.3.b   Indique el nombre y programa del estudiante con student_id = 76567
+SELECT name, program 
+FROM student
+WHERE student_id = 76567;
+
+
+--3.3.c   Encuentre los nombres de todos los estudiantes que se han matriculado en el curso con
+--        course_id = 837873
+
+
+
+--3.3.d   Cree una vista llamada better_students que presente los estudiantes que obtuvieron las
+--        notas más altas por cada semestre entre los años 1900 y 2018
+
+
+
+
+
