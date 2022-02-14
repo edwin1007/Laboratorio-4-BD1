23fbb122c90e5ada755fda49dcf60c8b51a0ee21
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2022-02-14 00:36:07 -0500

    se agrega drops.sql

--- drops.sql
+++ drops.sql
@@ -1,2 +1,30 @@
 
 /*elimar tablas, secuencias y triggers*/
+
+DROP PROCEDURE create_teaches;
+
+DROP TRIGGER invalid_grade_insert ON enrols;
+
+DROP TRIGGER same_grade_update ON enrols;
+
+DROP TRIGGER invalid_grade_update ON enrols;
+
+DROP TRIGGER invalid_grade_insert ON enrols;
+
+DROP TABLE requires;
+
+DROP TABLE teaches;
+
+DROP TABLE enrols;
+
+DROP TABLE course_offering;
+
+DROP TABLE course;
+
+DROP TABLE instructor;
+
+DROP TABLE student;
+
+DROP SEQUENCE course_seq;
+
+DROP SEQUENCE student_seq;
